8821f6f362e631d0604bd0e3997fabfc4048993b
radenska <yana_radenska@yahoo.com>
2017-03-02 13:13:17 -0800

    server up and running

---
+++ lab-yana/server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const debug = require('debug')('peak:server');
+const morgan = require('morgan');
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+// const mountainsRouter = require('./route/mountains-route.js');
+// const peakRouter = require('./route/peak-route,js');
+const PORT = 3003;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/peakapp';
+
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+const app = express();
+
+app.use(cors()); //make sure to CALL cors!!!
+app.use(morgan('dev'));
+// app.use(mountainsRouter);
+// app.use(peakRouter);
+// app.use(errors);
+
+app.listen(PORT, () => debug(`listening on port ${PORT}`));

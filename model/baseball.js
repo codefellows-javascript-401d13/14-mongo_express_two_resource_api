a9acfb043efe5a1af3a59a30cbb5101cbe9b3062
Dana Kulp <dkulp23@gmail.com>
2017-03-02 14:57:21 -0800

    Created secondary resource model psuedo constructor.

--- model/baseball.js
+++ model/baseball.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.schema;
+
+const baseballSchema = Schema({
+  year: { type: String, required: true },
+  player: { type: String, required: false},
+  cardId: { type: Schema.Types.ObjectId, required: true }
+});
+
+module.exports = mongoose.model('baseball', baseballSchema);
--- test/card-route-test.js
+++ test/card-route-test.js
@@ -5,3 +5,17 @@ const expect = require('chai').expect;
 const Card = require('../model/card.js');
 
 const PORT = process.env.PORT || 3000;
+const url = `http://localhost:${PORT}`;
+
+require('../server.js');
+
+const sampleCard = {
+  brand: 'Donruss',
+  completeSet: false,
+  single: true
+};
+
+const sampleBaseball = {
+  year: '1952',
+  player: 'Mickey Mantle'
+};

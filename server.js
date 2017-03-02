29ec8b679413ab520a8be7575147aeab29d60a40
Dana Kulp <dkulp23@gmail.com>
2017-03-02 14:43:11 -0800

    Updated card model module with method to add a relation to another resource.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const debug = require('debug')('card:server');
+const mongoose = require('mongoose');
+const errors = require('./lib/error-middleware.js');
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +13,8 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
+
+app.use(errors);
 
 app.listen(PORT, () => {
   debug(`Server's up on port: ${PORT}`);
--- test/card-route-test.js
+++ test/card-route-test.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const request = require('superagent');
+const expect = require('chai').expect;
+const Card = require('../model/card.js');
+
+const PORT = process.env.PORT || 3000;

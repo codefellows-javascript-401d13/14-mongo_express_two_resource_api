fa4253c616645b3152c94cfb9749525ae274d520
Dana Kulp <dkulp23@gmail.com>
2017-03-02 14:00:18 -0800

    Server created.

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const debug = require('debug')('card:server');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.listen(PORT, () => {
+  debug(`Server's up on port: ${PORT}`);
+});

085af3d33f51b04f70e58426044f3c52a7d155bc
Dana Kulp <dkulp23@gmail.com>
2017-03-02 16:56:06 -0800

    Wrote test and route for first resource get request.

--- test/card-route-test.js
+++ test/card-route-test.js
@@ -46,4 +46,42 @@ describe('Card Routes', function() {
       });
     });
   });
+
+  describe('GET: /api/card:id', function() {
+    describe('with a valid body', function() {
+      before( done => {
+        new Card(sampleCard).save()
+        .then( card => {
+          this.tempCard = card;
+          return Card.findByIdAndAddBaseball(card._id, sampleBaseball);
+        })
+        .then( baseball => {
+          this.tempBaseball = baseball;
+          done();
+        })
+        .catch(done);
+      });
+
+      after( done => {
+        if (this.tempCard) {
+          Card.remove({})
+          .then( () => done())
+          .catch(done);
+        }
+        done();
+      });
+
+      it('should return a card', done => {
+        request.get(`${url}/api/card/${this.tempCard._id}`)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.brand).to.equal(sampleCard.brand);
+          expect(res.body.baseball.length).to.equal(1);
+          expect(res.body.baseball[0].year).to.equal(sampleBaseball.year);
+          done();
+        });
+      });
+    });
+  });
 });

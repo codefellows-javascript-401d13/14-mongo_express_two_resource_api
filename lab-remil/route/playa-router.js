ebaeb5caa47e33cf879fa39d3e58e97f7326514d
Remil Marzan <remilonwheels@gmail.com>
2017-03-02 21:39:52 -0800

    complete Playa GET route and passed 200 test

--- lab-remil/route/playa-router.js
+++ lab-remil/route/playa-router.js
@@ -5,12 +5,21 @@ const debug = require('debug')('streetz:playa-router');
 const jsonParser = require('body-parser').json();
 
 const Squad = require('../model/squad.js');
+const Playa = require('../model/playa.js');
 const playaRouter = module.exports = new require('express').Router();
 
 playaRouter.post('/api/squad/:squadID/playa', jsonParser, function(req, res, next) {
   debug('POST: /api/squad/:squadID/playa');
 
   Squad.findByIdAndAddPlaya(req.params.squadID, req.body)
+  .then( playa => res.json(playa))
+  .catch(next);
+});
+
+playaRouter.get('/api/playa/:id', function(req, res, next) {
+  debug('GET: /api/playa/:id');
+
+  Playa.findById(req.params.id)
   .then( playa => res.json(playa))
   .catch(next);
 });
--- lab-remil/test/playa-router-test.js
+++ lab-remil/test/playa-router-test.js
@@ -53,6 +53,50 @@ describe('Playa Rizzoutes', function() {
           if (err) done(err);
           expect(res.status).to.equal(200);
           expect(res.body).to.have.property('_id');
+          expect(res.body.squadID).to.equal(this.tempSquad._id.toString());
+          expect(res.body.playaName).to.equal(bplaya.playaName);
+          expect(res.body.swagLevel).to.equal(bplaya.swagLevel);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('runnin\' a GET route @ /api/playa/:id', function() {
+    describe('with dat good id', function() {
+      before( done => {
+        new Squad(bsquad).save()
+        .then( squad => {
+          this.tempSquad = squad;
+          return Squad.findByIdAndAddPlaya(squad._id, bplaya);
+        })
+        .then( playa => {
+          this.tempPlaya = playa;
+          done();
+        })
+        .catch(done);
+      });
+
+      after( done => {
+        if (this.tempSquad) {
+          Promise.all([
+            Squad.remove({}),
+            Playa.remove({}),
+          ])
+          .then( () => done())
+          .catch(done);
+          return;
+        }
+        done();
+      });
+
+      it('betta return a playa', done => {
+        request.get(`${url}/api/playa/${this.tempPlaya._id}`)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.have.property('_id');
+          expect(res.body.squadID).to.equal(this.tempSquad._id.toString());
           expect(res.body.playaName).to.equal(bplaya.playaName);
           expect(res.body.swagLevel).to.equal(bplaya.swagLevel);
           done();
